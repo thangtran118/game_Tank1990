3b99da8c7b06c727f11e72b2dfe4c149dec2a63a
thangtran118 <thangtc.work@gmail.com>
2021-10-25 15:16:29 +0700

    home sprite

---
+++ modules/sprites/home.py
@@ -0,0 +1,20 @@
+import pygame
+
+# căn cứ - home
+
+class Home(pygame.sprite.Sprite):
+    def __init__(self, position, image_paths ,**kwargs):
+        self.imagepaths = image_paths
+        self.image = pygame.image.load(self.imagepaths[0])
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = position
+        self.alive = True
+
+    # home be destroyed
+    def setDead(self):
+        self.image = pygame.image.load(self.imagepaths[1])
+        self.alive = False
+
+    # draw home on screen
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
